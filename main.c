220eec9d76e3920e08a86d5b3327919b810c9216
Levi Santos <96695555+levisantosp@users.noreply.github.com>
2023-05-07 21:03:17 -0300

    learning how to debug a code 🥵

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("1\n");
+	printf("2\n");
+	printf("3\n");
+	printf("4\n");
+	printf("5\n");
+	system("pause");
+}
